fbe8891e903129a8628267ca668faf2f9114d6a2
Toli Leonovich <toli@leonovich.com>
2025-12-03 23:08:01 -0800

    feat(go): add YAML output formatting

---
+++ go/pkg/portainer/output.go
@@ -0,0 +1,45 @@
+package portainer
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type ErrorOutput struct {
+	Error ErrorDetail `yaml:"error"`
+}
+
+type ErrorDetail struct {
+	Code    string `yaml:"code"`
+	Message string `yaml:"message"`
+}
+
+func PrintYAML(data interface{}) error {
+	enc := yaml.NewEncoder(os.Stdout)
+	enc.SetIndent(2)
+	if err := enc.Encode(data); err != nil {
+		return APIError(fmt.Sprintf("failed to serialize: %s", err))
+	}
+	return nil
+}
+
+func PrintError(err error) {
+	pe, ok := err.(*PortainerError)
+	if !ok {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		return
+	}
+
+	output := ErrorOutput{
+		Error: ErrorDetail{
+			Code:    string(pe.Code),
+			Message: pe.Message,
+		},
+	}
+
+	enc := yaml.NewEncoder(os.Stderr)
+	enc.SetIndent(2)
+	enc.Encode(output)
+}
